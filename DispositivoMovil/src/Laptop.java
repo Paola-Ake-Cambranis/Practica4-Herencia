02bfd1e50171e51d799d771ff4dc1966dbc90f5c
Paola-Ake-Cambranis <148833125+Paola-Ake-Cambranis@users.noreply.github.com>
2023-10-24 19:30:47 -0600

    Primera parte

---
+++ DispositivoMovil/src/Laptop.java
@@ -0,0 +1,10 @@
+public class Laptop extends DispositivoMovil{
+    public Laptop(){
+        velocidadProcesador = 2.47f;
+        cargaBateria = 100;
+        memoria = 4096;
+        sistemaOperativo = "Microsoft Windows 7";
+        marca = "HP";
+        modelo = "Pavilion dv5-2247la";
+    }
+}
---
+++ DispositivoMovil/src/TestDispositivo.java
@@ -0,0 +1,8 @@
+public class TestDispositivo {
+	public static void main(String[] args){
+		Laptop lap = new Laptop();
+		lap.encender();
+		lap.verCargaBateria();
+		lap.apagar();
+	}
+}
